bf531f3ae8f03929f46a7a4916e63592a4cd2a9e
exlntz <chempionat34@gmail.com>
2025-12-20 14:42:49 +0300

    startttt

--- app/main.py
+++ app/main.py
@@ -1,28 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 
 app=FastAPI()
 
 @app.get('/')
 def home() -> str:
-    return '1236'
+    return 'сама платформа'
 
-
-
-
-posts=[
-    {'id': 1, 'tittle': 'News 1', 'body': 'Text 1'},
-    {'id': 2, 'tittle': 'News 2', 'body': 'Text 2'},
-    {'id': 3, 'tittle': 'News 3', 'body': 'Text 3'}
-]
-
-
-@app.get('/items')
-async def items() -> list:
-    return posts
-
-@app.get('/items/{id}')
-async def items(id: int):
-    for post in posts:
-        if post['id'] == id:
-            return post
-    return 'Not found'
