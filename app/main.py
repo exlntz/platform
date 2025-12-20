608fbe31e9159214e3a4d63f8b3155015a3cd590
exlntz <chempionat34@gmail.com>
2025-12-21 00:10:51 +0300

    vaild

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, EmailStr
+import uvicorn
 
 app=FastAPI()
 
+data={
+    'id': 1,
+    'name': 'John',
+    'mail': 'aomosm@mail.ru',
+    'bio': None,
+    'elo': 1000
+}
+class UserSchema(BaseModel):
+    id: int
+    name: str = Field(max_length=50)
+    mail: EmailStr
+    bio: str | None
+    elo: int = Field(ge=0)
+
 @app.get('/')
 def home() -> str:
-    return 'сама платформа'
+    return 'главная страница'
 
+users=[]
+@app.post('/users',tags=['Пользователи'],summary='Создание пользователя')
+def create_user(user: UserSchema):
+    users.append(user)
+    return 'True'
+
+
+
+if __name__=='__main__':
+    uvicorn.run('app.main:app',reload=True)
