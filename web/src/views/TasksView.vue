9801772c6199a9c074204ec102d568fccbed2aa6
Maksim <korolew.max@gmail.com>
2025-12-25 16:05:21 +0300

    vue + vite

--- web/src/views/TasksView.vue
+++ web/src/views/TasksView.vue
@@ -7,7 +7,27 @@ export default {
 <template>
   <div class="Tasks">
     <h1>Tasks list</h1>
+    <div class="filters">
+      <select>
+        <option value="">Все предметы</option>
+        <option value="math">Математика</option>
+        <option value="it">Информатика</option>
+      </select>
+      <select>
+        <option value="">Любая сложность</option>
+        <option value="easy">Легкая</option>
+        <option value="hard">Сложная</option>
+      </select>
+    </div>
+    <div class="task-list">
+        <div v-for="task in filteredTasks" :key="task.id" class="task-card">
+          <h3>{{ task.title }}</h3>
+          <p>Тема: {{ task.topic }} | Сложность: {{ task.difficulty }}</p>
+          <button @click="startTask(task.id)">Решать</button>
+        </div>
+    </div>
   </div>
+
 </template>
 
 <style scoped>
