f877221681e1b5c5038943e90e95217441f53d8b
Maksim <korolew.max@gmail.com>
2025-12-26 13:37:43 +0300

    reg page done

--- web/src/views/RegisterView.vue
+++ web/src/views/RegisterView.vue
@@ -1,31 +1,59 @@
 <script>
+import axios from 'axios'
+import router from '../router/index.js'
 export default {
-  name: "RegisterView"
+  name: "RegisterView",
+  data(){
+    return {
+      regUsername:'',
+      regEmail:'',
+      regPassword:'',
+      regPasswordRepeat:''
+    }
+  },
+  methods:{
+    async register(){
+      if (this.regPassword===this.regPasswordRepeat) {
+        try {
+          const response = await axios.post('http://127.0.0.1:8000/auth/register', {
+            username: this.regUsername,
+            email: this.regEmail,
+            password: this.regPassword,
+            password_repeat: this.regPasswordRepeat
+          });
+          console.log('Register succes' + response.data)} catch(err){alert(err)}
+          router.push('/auth')
+      }else{
+        alert('Password is incorrect')
+      }
+    }
+  }
 }
 </script>
 
 <template>
   <div class="register">
     <h1>Registration</h1>
-    <form @submit.prevent="register"></form>
-    <div>
-      <label for="regUsername">Username:</label>
-      <input type="text" id="regUsername" v-model="regUsername">
-    </div>
-    <div>
-      <label for="regEmail">email:</label>
-      <input type="text" id="regEmail" v-model="regEmail">
-    </div>
-    <div>
-      <label for="regPassword">password:</label>
-      <input type="text" id="regPassword" v-model="regPassword">
-    </div><div>
-    <label for="regPasswordRepeat">repeat password:</label>
-    <input type="text" id="regPasswordRepeat" v-model="regPasswordRepeat">
+    <form @submit.prevent="register">
+      <div>
+        <label for="regUsername">Username:</label>
+        <input type="text" id="regUsername" v-model="regUsername">
+      </div>
+      <div>
+        <label for="regEmail">email:</label>
+        <input type="text" id="regEmail" v-model="regEmail">
+      </div>
+      <div>
+        <label for="regPassword">password:</label>
+        <input type="text" id="regPassword" v-model="regPassword">
+      </div>
+      <div>
+        <label for="regPasswordRepeat">repeat password:</label>
+        <input type="text" id="regPasswordRepeat" v-model="regPasswordRepeat">
+      </div>
+      <button type="submit">reg in</button>
+    </form>
   </div>
-    <button type="submit">reg in</button>
-  </div>
-
 </template>
 
 <style scoped>
