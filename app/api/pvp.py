8b1ad6d187820e301e91654b9294420f83e99a0f
exlntz <chempionat34@gmail.com>
2026-02-01 19:28:34 +0300

    pvp ws

--- app/api/pvp.py
+++ app/api/pvp.py
@@ -332,7 +332,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/pvp/join");
+            var ws = new WebSocket("wss://olymp-platform.ru/api/pvp/join");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
